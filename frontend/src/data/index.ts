70ca4fa8b6198b189a8f2b8820094251adeddb52
Brandon Jimenez <brandon.jimenez@techtonic.com>
2020-07-20 13:53:12 -0600

    Better Data export

--- frontend/src/data/index.ts
+++ frontend/src/data/index.ts
@@ -2,8 +2,17 @@ import equipment from './equipment.csv';
 import sites from './sites.csv';
 import gangs from './gangs.csv';
 
+import { Equipable } from '@/model/Equipment';
+import { Site } from '@/model/Sector';
+import { Gang } from '@/model/Gang';
+
+export const Equipment: Equipable[] = equipment;
+export const Sites: Site[] = sites;
+export const Gangs: Gang[] = gangs;
+
 export default {
-  equipment,
-  sites,
-  gangs,
+  Equipment,
+  Sites,
+  Gangs,
 };
+
