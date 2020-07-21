44923685845829dd73b3d6a8717b0180d7c86591
Brandon Jimenez <brandon.jimenez@techtonic.com>
2020-07-21 16:51:58 -0600

    Styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,13 +4,13 @@
   </div>
 </template>
 
-<style lang="stylus">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+export default {
+
 }
+</script>
+
+<style lang="stylus">
+#app {}
+
 </style>
