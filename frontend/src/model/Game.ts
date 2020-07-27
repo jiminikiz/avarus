d1c63e2dcbf7fa2169a5bac78f3ea9f9b21a6432
Brandon Jimenez <brandon.jimenez@techtonic.com>
2020-07-26 21:48:02 -0600

    KISS - Move initialization logic all into GAME

--- frontend/src/model/Game.ts
+++ frontend/src/model/Game.ts
@@ -1,9 +1,12 @@
-import { Gangs } from '@/data';
+import { Gangs, Sites } from '@/data';
+
+import { Tools } from '@/lib/Tools';
+import { Board, BoardTile } from '@/lib/Board';
 import { Deck } from '@/lib/Deck';
-import { Board } from '@/lib/Board';
-import { GameBoard } from './GameBoard';
 
 import { Player } from './Player';
+import { Sector, Site } from './Sector';
+import { Gang } from './Gang';
 
 export enum GameMode {
   Greed = 'Greed',
@@ -43,28 +46,45 @@ export class Game {
   public timeLimit: number = Infinity;
   public board: Board;
   public decks: Map<string, Deck> = new Map();
+  public sectors: Map<string, Sector> = new Map();
 
   constructor(options: GameOptions) {
     // TODO: Make Dynamic, good for now
     const { rows, cols } = options.board;
     const GangsDeck = new Deck({ name: 'Gangs', cards: Gangs });
 
-    this.setDecks([GangsDeck]);
-    this.setPlayers(options.players, GangsDeck);
-    this.players = options.players;
     this.mode = options.mode;
     this.difficulty = options.difficulty;
-    this.board = new GameBoard({ rows, cols });
+    this.board = new Board({ rows, cols });
+    this.players = options.players;
+
+    this.setDecks([GangsDeck]);
+    this.dealCards(options.players, GangsDeck);
+    this.generateSectors();
+
   }
 
   private setDecks(decks: Deck[]): void {
     decks.forEach((deck: Deck) => this.decks.set(deck.name, deck));
   }
 
-  private setPlayers(players: Player[], deck: Deck): void {
-    const deal = deck.deal({
-      handSize: 3,
-      playerCount: players.length,
-    });
+  private dealCards(players: Player[], deck: Deck): void {
+    const dealt = deck.deal({ handSize: 3, playerCount: players.length });
+    players.forEach(player => player.hand = dealt.pop() as Gang[]);
   }
+
+  private randomSites(cardinality: number = 3): Site[] {
+    return Tools.random.elements(cardinality, Sites) as Site[];
+  }
+
+  private generateSector(tile: BoardTile, key: string): void {
+    const sites = this.randomSites();
+    const sector = new Sector({ ...tile, sites });
+    this.sectors.set(key, sector);
+  }
+
+  private generateSectors(): void {
+    this.board.tiles.forEach(this.generateSector.bind(this));
+  }
+
 }
